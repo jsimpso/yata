96bd82897f331e7b6fe84780d82d60e85092dd3d
James Simpson <james.simpson@canonical.com>
2025-04-15 11:10:46 +0800

    Tutorial step #8
    * Add failing tests

--- todos/tests/test_todos.py
+++ todos/tests/test_todos.py
@@ -1,5 +1,5 @@
 from django.urls import reverse
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 
 def test_display_empty_list_on_first_load(live_server, page: Page):
@@ -12,3 +12,26 @@ def test_display_empty_list_on_first_load(live_server, page: Page):
 def reverse_url(live_server, viewname, urlconf=None, args=None, kwargs=None, current_app=None):
     end = reverse(viewname, urlconf, args, kwargs, current_app)
     return f"{live_server.url}{end}"
+
+
+def test_create_todo_item_shows_new_item(live_server, page: Page):
+    url = reverse_url(live_server, "index")
+
+    page.goto(url)
+    page.get_by_role("textbox", name="Title*").click()
+    page.get_by_role("textbox", name="Title*").fill("testing")
+    page.get_by_role("button", name="Add").click()
+    page.wait_for_selector("text=testing")
+
+
+def test_no_empty_text_when_todo_list_not_empty(live_server, page: Page):
+    url = reverse_url(live_server, "index")
+
+    page.goto(url)
+    page.get_by_role("textbox", name="Title*").click()
+    page.get_by_role("textbox", name="Title*").fill("testing")
+    page.get_by_role("textbot", name="Title*")
+    page.get_by_role("textbox", name="Description*").click()
+    page.get_by_role("textbox", name="Description*").fill("testing")
+    page.get_by_role("button", name="Add").click()
+    expect(page.get_by_text("Nothing to see here...")).not_to_be_visible()
